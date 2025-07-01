f8040b2d9768ea9eaae027330589dd20ed6cb3a5
S.Berkebile <samuel.berkebile@gmail.com>
2025-06-30 23:42:48 -0500

    totally revamped

--- scripts/world.gd
+++ scripts/world.gd
@@ -8,6 +8,7 @@ var target_pos: Vector2 = Vector2.ZERO
 
 func _input(event: InputEvent) -> void:
 
+	# Crapy camera controlls
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed:
 		is_panning = true
 		last_mouse_position = get_global_mouse_position()
@@ -20,6 +21,6 @@ func _input(event: InputEvent) -> void:
 		last_mouse_position = current_mouse_position
 	
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_UP:
-		camera.zoom *= 1.1
+		camera.zoom *= 1.05
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-		camera.zoom *= 0.9
+		camera.zoom *= 0.95
