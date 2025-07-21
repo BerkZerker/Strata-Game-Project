2ae5d317f8f3a1086a44e61f91aff3d5c7660fab
S.Berkebile <samuel.berkebile@gmail.com>
2025-07-21 14:15:41 -0500

    setup

--- game.gd
+++ game.gd
@@ -1,55 +1,32 @@
 @tool
 extends Node2D
 
-const MarchingSquares = preload("res://marching_squares.gd")
+const GreedyMeshing = preload("res://src/greedy_meshing.gd")
 
 func _ready() -> void:
-	var iso_level = 0.3
+	# Set up some noise for the terrain generation
 	var noise = FastNoiseLite.new()
 	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
 	noise.seed = 1
 	noise.frequency = 0.08
 	
-	var chunks = []
+	var world = []
 	var chunk_size = 16
+	var width = 2
+	var height = 2
+	var scale = 10
 
-	for x in range(64):
-		if x % chunk_size == 0:
-			chunks.append([])
-		for y in range(64):
-			var value = noise.get_noise_2d(x, y)
-			#if value < iso_level:
-				#value = 0
-			#else:
-				#value = 1
-			chunks[x / chunk_size].append(value)
-			
-			
-	var marching_squares = MarchingSquares.new()
-
-	#var chunk_data = []
-	#for chunk in chunks:
-		#var vertices = marching_squares.generate_vertices(chunk, iso_level, 10, Vector2(x / chunk_size, 0), chunk_size)
-		#chunk_data.append(vertices)
+	for x in range(width):
+		world.append([]) # Add a new row to the world array
+		for y in range(height):
+			# Generate a chunk of noise data
+			var chunk = []
+			for i in range(chunk_size):
+				chunk.append([])
+				for j in range(chunk_size):
+					var value = noise.get_noise_2d((x * chunk_size + i), (y * chunk_size + j))
+					chunk[i].append(value)
+			# Add the chunk to the world array
+			world[x].append(chunk)
 	
-	#var vertices = marching_squares.generate_vertices(vol_data, iso_level, 10)
-	#var polygons = marching_squares.bake_polygons(vertices)
-
-	var line_drawer = $Node2D
-	
-	for vertices in chunks:
-		for segment in vertices:
-			if segment.size() == 2:
-				var line = Line2D.new()
-				line.width = 1
-				line.default_color = Color(1, 0, 0)
-				var point_1 = Vector2(segment[0][0], segment[0][1])
-				var point_2 = Vector2(segment[1][0], segment[1][1])
-				line.add_point(point_1)
-				line.add_point(point_2)
-				line_drawer.add_child(line)
-	
-	#for p in polygons:
-		#var polygon = CollisionPolygon2D.new()
-		#polygon.polygon = p
-		#$Node2D/StaticBody2D.add_child(polygon)
+	$
