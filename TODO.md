c9b6de48d6ed920a6df223eac677c2b89ba6b1de
S.Berkebile <samuel.berkebile@gmail.com>
2025-08-24 19:46:34 -0500

    worked on player.gd and added block step

--- TODO.md
+++ TODO.md
@@ -1,17 +1,17 @@
 # TODO List
 
+- Fix the stupid player.gd file and write my own collision logic. This should handle gravity, velocity, jumping, collisions, step up on blocks, and coyote jump.
+
+- Create an input manager node as part of the game scene. This should be able to talk to the player and world scenes.
+
 - Add chunk_manager scene and add it to the world. It handles the threaded loading and unloading of chunks. This file will also be in charge of generating the chunk data. This may mean I should make the generator a static function.
 
 - World scene and script cleanup
 
-- Create an input manager node perhaps?
-
-- Player scene and script; go ahead and add a raycast or something and make the collision shape a rect. Do my own slope handling logic. Move the input code for editing the terrain from the world script
+  Note! Test the `setup_collision_shapes` function with debugging print statements after editing is added back in to make sure that it works as intended.
 
 - Update `README.md` and add some basic documentation and at least 1 screenshot
 
 - Research algorithms to generate tessellating shapes as cells in the terrain using data driver attributes such as roughness, scale variance, and starting shape. Maybe voronoi diagrams & Lloyd's relaxation?
 
 - Implement a flood fill algorithm for terrain modification based on the cell terrain data. Start with breaking.
-
-  Note! Test the `setup_collision_shapes` function with debugging print statements after editing is added back in to make sure that it works as intended.
--- src/entities/player.gd
+++ src/entities/player.gd
@@ -1,21 +1,71 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
+@export var SPEED: int = 100
+@export var JUMP_VELOCITY: int = -250
+@export var STEP_HEIGHT: int = 3
+@export var COYOTE_TIME: float = 0.2
+@export var ZOOM_AMOUNT: float = 0.1
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var camera: Camera2D = $Camera2D
+@onready var left_shapecast: ShapeCast2D = $CollisionShape2D/ShapeCastLeft
+@onready var right_shapecast: ShapeCast2D = $CollisionShape2D/ShapeCastRight
+@onready var coyote_timer: Timer = $CoyoteTimer
+
+var was_on_floor: bool = false
+
+func _ready() -> void:
+	coyote_timer.wait_time = COYOTE_TIME
 
 
-# I know this code is a mess I just don't care
-# - past me
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+		# Start coyote timer when walking off a ledge
+		if was_on_floor and coyote_timer.is_stopped():
+			coyote_timer.start()
+	else:
+		# Reset coyote timer when on floor
+		coyote_timer.stop()
+		
+	was_on_floor = is_on_floor() # Update floor state
+
+	# Handle stepping up slopes
+	handle_step_up()
 
 	# Move the player.
 	move_and_slide()
+	
+	# Just write my own collision logic since this is such a specialized geometry
+	# I can reuse much of what I already have, such as coyote jump, and the wall step but 
+	# I need to manually move the player and handle collisions if they occur.
+	
+	#var data = move_and_collide(velocity)
+
+
+func handle_step_up() -> void:
+	# Only check for steps if we're moving and on the floor
+	if velocity.x != 0 and is_on_floor():
+		var check_shapecast = right_shapecast if velocity.x > 0 else left_shapecast
+
+		# Check if there's an obstacle in front
+		if check_shapecast.is_colliding():
+			var original_pos = check_shapecast.position
+
+			for i in range(STEP_HEIGHT):
+				check_shapecast.position.y -= 1
+				check_shapecast.force_shapecast_update()
+
+				if not check_shapecast.is_colliding():
+					# We can step up! Move the player up
+					was_on_floor = true
+					coyote_timer.stop()
+					position.y -= i + 1
+					break
+
+			check_shapecast.position = original_pos
 
 
 # need to move all the input handling to this func at some point
@@ -23,23 +73,22 @@ func _input(event: InputEvent) -> void:
 	# Handle zoom
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			camera.zoom.x += 0.1
-			camera.zoom.y += 0.1
+			camera.zoom.x += ZOOM_AMOUNT
+			camera.zoom.y += ZOOM_AMOUNT
 
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			camera.zoom.x -= 0.1
-			camera.zoom.y -= 0.1
+			camera.zoom.x -= ZOOM_AMOUNT
+			camera.zoom.y -= ZOOM_AMOUNT
 
-	# Handle jump.
-	if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or not coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
+		coyote_timer.stop()
 
 	# Get the input direction and handle the movement/deceleration.
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_action_pressed("move_left"):
 		velocity.x = - SPEED
-
-	elif Input.is_key_pressed(KEY_D):
+	elif Input.is_action_pressed("move_right"):
 		velocity.x = SPEED
-
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
