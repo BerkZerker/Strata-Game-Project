d54ccbcf9b743180edf7ac0d3ca93cb477459a53
S.Berkebile <samuel.berkebile@gmail.com>
2025-09-15 08:29:26 -0500

    working on stuff

--- global_settings.gd
+++ global_settings.gd
@@ -1,5 +1,6 @@
 extends Node
 
 const CHUNK_SIZE: int = 64
+const CHUNK_PADDING: int = 64
 const TILE_SIZE: int = 1
 const LOD_RADIUS: int = 4
--- src/world/terrain/chunk.gd
+++ src/world/terrain/chunk.gd
@@ -1,9 +1,6 @@
 class_name Chunk extends Node2D
 
-@export var CHUNK_PADDING: int = 64 # How many tiles to pad the chunk by to detect entities
-
 @onready var visual_mesh: MeshInstance2D = $TerrainMesh
-@onready var area_2d: Area2D = $Area2D
 @onready var static_body: StaticBody2D = $StaticBody2D
 
 var terrain_data: Array = []
@@ -23,32 +20,12 @@ func _ready() -> void:
 # Should be called after the node is initialized and added to the tree. 
 # Should only be called once, after that use rebuild()
 func build() -> void:
-	_setup_area_2d()
 	_setup_collision_shapes()
 	_setup_visual_mesh()
-
-
-# Since arrays are passed by reference, any changes I make to the terrain data
-# outside of this script should be reflected on the terrain_data. This means
-# all this function has to do is refresh the collision shapes and the visual mesh.
-func rebuild() -> void:
-	_setup_collision_shapes()
-	_setup_visual_mesh()
-
-
-# Sets up the area2d to detect entities and activate the chunk when one is nearby.
-func _setup_area_2d() -> void:
-	var collision_shape = CollisionShape2D.new()
-	var shape = RectangleShape2D.new()
-
-	# Fancy maths to scale and position it
-	shape.size = Vector2(chunk_size + CHUNK_PADDING, chunk_size + CHUNK_PADDING)
-	collision_shape.position = Vector2(shape.size.x / 2.0 - CHUNK_PADDING / 2.0, shape.size.y / 2.0 - CHUNK_PADDING / 2.0)
-	collision_shape.shape = shape
-	area_2d.add_child(collision_shape)
 
 
 # Helper function to setup the collision shapes.
+# This starts with the shapes disabled.
 func _setup_collision_shapes() -> void:
 	# Run greedy meshing on the local copy of the terrain data
 	var shapes = GreedyMeshing.mesh(terrain_data)
@@ -57,23 +34,10 @@ func _setup_collision_shapes() -> void:
 	for child in static_body.get_children():
 		child.queue_free()
 
-	# Check if the chunk is already active
-	var is_active = false
-	for body in area_2d.get_overlapping_bodies():
-		if body is CharacterBody2D: # For now this is just the player
-			is_active = true
-			print('body detected during setup! enabled shapes') # This will be helpful when editing terrain
-			break
-
 	# And setup the shapes
-	if is_active:
-		for shape in shapes:
-			shape.set_deferred("disabled", false)
-			static_body.add_child(shape)
-	else:
-		for shape in shapes:
-			shape.set_deferred("disabled", true)
-			static_body.add_child(shape)
+	for shape in shapes:
+		shape.set_deferred("disabled", true)
+		static_body.add_child(shape)
 
 
 func _setup_visual_mesh():
@@ -95,17 +59,3 @@ func _setup_visual_mesh():
 
 	var data_texture = ImageTexture.create_from_image(image)
 	visual_mesh.material.set_shader_parameter("chunk_data_texture", data_texture)
-
-
-# Enables the collision shapes when the player enters the area
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D: # For now this is just the player
-		for child in static_body.get_children():
-			child.set_deferred("disabled", false)
-
-
-# Disables the collision shapes when the player exits the area
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is CharacterBody2D: # For now this is just the player
-		for child in static_body.get_children():
-			child.set_deferred("disabled", true)
