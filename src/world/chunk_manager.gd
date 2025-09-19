24f94829f7eeae4a9f7ef2b85c92523aa24e67b8
S.Berkebile <samuel.berkebile@gmail.com>
2025-09-19 14:55:47 -0500

    better threading & recycling (SUPER BUGGY)

--- src/world/chunk_manager.gd
+++ src/world/chunk_manager.gd
@@ -3,21 +3,28 @@ class_name ChunkManager extends Node2D
 # Variables
 @export var WORLD_SEED: int = randi() % 1000000 # Random seed for the world generation
 
-var chunks: Dictionary = {}
-
 var _terrain_generator: TerrainGenerator
 var _thread: Thread
 var _mutex: Mutex
 var _semaphore: Semaphore
 
-var _generation_queue: Array[Vector2i] = []
-var _removal_queue: Array[Vector2i] = []
+var _current_player_pos: Vector2i
+var _old_player_pos: Vector2i
+
+# Used by multiple threads
+var chunks: Dictionary[Vector2i, Chunk] = {} # Dictionary of Vector2i to Chunk objects
+var _thread_alive: bool = true
+var _generation_queue: Array[Vector2i] = [] # Positions of chunks to generate
+var _removal_queue: Array[Chunk] = [] # Chunks to remove
+var _recycle_pool: Array[Chunk] = [] # Pool of chunks to reuse
 
 
 # Setup the chunk manager
 func _ready() -> void:
+	# Make a new terrain generator with the given seed
 	_terrain_generator = TerrainGenerator.new(WORLD_SEED)
 
+	# Set up the thread stuff
 	_mutex = Mutex.new()
 	_semaphore = Semaphore.new()
 	_thread = Thread.new()
@@ -27,69 +34,157 @@ func _ready() -> void:
 	SignalBus.player_chunk_changed.connect(_on_player_chunk_changed)
 
 
+# Gracefully stop the thread when the node is removed from the scene tree
+func _exit_tree():
+	_mutex.lock()
+	_thread_alive = false # Tell the thread it can shut down permanently
+	_mutex.unlock()
+	_semaphore.post() # Wake the thread if it's waiting
+	_thread.wait_to_finish()
+
+
+# The main loop for the worker thread to process chunk updates
 func _process_chunk_updates() -> void:
-	while true:
+	while _thread_alive: # Keep the thread alive until told to stop
+		# Wait for work to do
 		_semaphore.wait()
-		_mutex.lock()
-		# Process chunk updates here if needed
-		# Do all looping over the queues and updates to the arrays inside the mutex lock
 
-		for pos in _removal_queue:
-			if chunks.has(pos):
-				chunks[pos].call_deferred("queue_free")
-				chunks.erase(pos)
-		_removal_queue.clear()
-
-		for pos in _generation_queue:
-			var chunk_data = _terrain_generator.generate_chunk(pos)
-			var chunk_scene = load("uid://dbbq2vtjx0w0y")
-			var new_chunk = chunk_scene.instantiate()
-			new_chunk.generate(chunk_data, pos)
-			new_chunk.build()
-			chunks[pos] = new_chunk
-			call_deferred("add_child", new_chunk)
-		_generation_queue.clear()
-
-		_mutex.unlock()
-		
-
-func _on_player_chunk_changed(new_player_pos: Vector2i) -> void:
-	# Calculate the bounds of chunks that should be loaded
-	var min_x = new_player_pos.x - GlobalSettings.LOAD_RADIUS
-	var max_x = new_player_pos.x + GlobalSettings.LOAD_RADIUS
-	var min_y = new_player_pos.y - GlobalSettings.LOAD_RADIUS
-	var max_y = new_player_pos.y + GlobalSettings.LOAD_RADIUS
-
-	# Add chunks to removal queue if they are out of bounds
-	for chunk_pos in chunks:
-		if chunk_pos.x < min_x or chunk_pos.x > max_x or chunk_pos.y < min_y or chunk_pos.y > max_y:
-			# Queue chunk for removal
+		# Work on generating chunks
+		while _generation_queue.size() > 0 and _thread_alive:
 			_mutex.lock()
-			_removal_queue.append(chunk_pos)
+			var pos = _generation_queue.pop_back()
+			_mutex.unlock()
+			if pos == null:
+				break
+
+			# Try to recycle, otherwise make a new chunk
+			if _recycle_pool.size() > 0:
+				# Recycle a chunk from the pool, without removing it from the scene tree
+				_mutex.lock()
+				var recycled_chunk = _recycle_pool.pop_back()
+				_mutex.unlock()
+				if recycled_chunk == null:
+					continue
+				
+				# Generate new data for the chunk
+				var new_chunk_data = _terrain_generator.generate_chunk(pos)
+				var new_collision_shapes = GreedyMeshing.mesh(new_chunk_data)
+
+				# Thread safe versions of generate and build
+				recycled_chunk.regenerate(new_chunk_data, new_collision_shapes, pos)
+				recycled_chunk.rebuild()
+
+				# And add the recycled chunk to the chunks dictionary
+				_mutex.lock()
+				chunks[pos] = recycled_chunk
+				_mutex.unlock()
+
+			else: # Create a new chunk
+				var chunk_scene = load("uid://dbbq2vtjx0w0y") # Thread safe and only used if we can't recycle.
+				var new_chunk = chunk_scene.instantiate() # Ditto above
+
+				# Generate data for the chunk
+				var chunk_data = _terrain_generator.generate_chunk(pos)
+				var collision_shapes = GreedyMeshing.mesh(chunk_data)
+
+				# Threaded versions of generate and build
+				new_chunk.generate(chunk_data, collision_shapes, pos)
+				new_chunk.build()
+
+				# And add the new chunk to the chunks dictionary
+				_mutex.lock()
+				chunks[pos] = new_chunk
+				_mutex.unlock()
+
+				# Finally, add the new chunk to the scene tree on the main thread
+				call_deferred("add_child", new_chunk)
+		
+		# Check if we still have chunks left over and queue them for deletion
+		if _recycle_pool.size() > GlobalSettings.RECYCLE_POOL_MAX_SIZE:
+			_mutex.lock()
+			for i in range(_recycle_pool.size() - GlobalSettings.RECYCLE_POOL_MAX_SIZE):
+				var chunk_to_remove = _recycle_pool.pop_back()
+				_removal_queue.append(chunk_to_remove)
 			_mutex.unlock()
 
-	# Calculate the bounds to enable collision shapes
-	var collision_min_x = new_player_pos.x - GlobalSettings.COLLISION_RADIUS
-	var collision_max_x = new_player_pos.x + GlobalSettings.COLLISION_RADIUS
-	var collision_min_y = new_player_pos.y - GlobalSettings.COLLISION_RADIUS
-	var collision_max_y = new_player_pos.y + GlobalSettings.COLLISION_RADIUS
 
-	# Generate new chunks within radius
-	for x in range(min_x, max_x + 1):
-		for y in range(min_y, max_y + 1):
-			var pos = Vector2i(x, y)
+# Called when the player changes chunk position, updates _current_player_pos
+# This will start the chunk updates in _process
+func _on_player_chunk_changed(new_player_pos: Vector2i) -> void:
+	_current_player_pos = new_player_pos
 
-			# Check if chunk doesn't exist and create it
-			if not chunks.has(pos):
-				# Generate new chunk
-				_mutex.lock()
-				_generation_queue.append(pos)
-				_mutex.unlock()
-			else:
-				# Enable or disable collision based on distance to player
-				if pos.x >= collision_min_x and pos.x <= collision_max_x and pos.y >= collision_min_y and pos.y <= collision_max_y:
-					chunks[pos].enable_collision()
+
+func _process(_delta: float) -> void:
+	# Check if the player has moved to a new chunk
+	if _current_player_pos != _old_player_pos:
+		# Update old player position
+		_old_player_pos = _current_player_pos
+		# Calculate the bounds of chunks that should be loaded
+		var load_min_x = _current_player_pos.x - GlobalSettings.LOAD_RADIUS
+		var load_max_x = _current_player_pos.x + GlobalSettings.LOAD_RADIUS
+		var load_min_y = _current_player_pos.y - GlobalSettings.LOAD_RADIUS
+		var load_max_y = _current_player_pos.y + GlobalSettings.LOAD_RADIUS
+		# Calculate the bounds to enable collision shapes
+		var collision_min_x = _current_player_pos.x - GlobalSettings.COLLISION_RADIUS
+		var collision_max_x = _current_player_pos.x + GlobalSettings.COLLISION_RADIUS
+		var collision_min_y = _current_player_pos.y - GlobalSettings.COLLISION_RADIUS
+		var collision_max_y = _current_player_pos.y + GlobalSettings.COLLISION_RADIUS
+
+		# STEP 1: Add out of bounds chunks to the recycle pool
+		var recycle_queue: Array = []
+		# Add chunks to the recycle pool if they are out of bounds
+		for chunk_pos in chunks:
+			if chunk_pos.x < load_min_x or chunk_pos.x > load_max_x or chunk_pos.y < load_min_y or chunk_pos.y > load_max_y:
+				# Add to recycle queue
+				recycle_queue.append(chunk_pos)
+
+		# Do this to avoid modifying the dictionary while iterating over it
+		for pos in recycle_queue:
+			_recycle_pool.append(chunks[pos]) # Add the chunk to the recycle pool
+			_mutex.lock()
+			chunks.erase(pos) # Remove it from the active chunks
+			_mutex.unlock()
+
+		# STEP 2: Queue new chunks to be generated / recycled
+		# Generate new chunks within radius
+		for x in range(load_min_x, load_max_x + 1):
+			for y in range(load_min_y, load_max_y + 1):
+				var pos = Vector2i(x, y)
+
+				# Check if chunk doesn't exist and create it
+				if not chunks.has(pos):
+					# Generate new chunk
+					_mutex.lock()
+					_generation_queue.append(pos)
+					_mutex.unlock()
+				
 				else:
-					chunks[pos].disable_collision()
-	
+					# STEP 3: Update collision shapes to only activate chunks near the player
+					# Enable or disable collision based on distance to player
+					# Note that `enable_collision` and `disable_collision` are called on the main thread via
+					# call_deferred, and thus are thread safe
+					if pos.x >= collision_min_x and pos.x <= collision_max_x and pos.y >= collision_min_y and pos.y <= collision_max_y:
+						chunks[pos].enable_collision()
+					else:
+						chunks[pos].disable_collision()
+
+		# STEP 4: Queue chunks for removal if they aren't needed by the recycle pool
+		if _removal_queue.size() > 0:
+			# Free the chunks in the removal queue
+			for chunk in _removal_queue:
+				chunk.queue_free()
+			_mutex.lock()
+			_removal_queue.clear()
+			_mutex.unlock()
+
+		# STEP 5: Wake the worker thread
+		_semaphore.post()
+
+
+# PLACEHOLDER
+# Just add the new chunk to the generation queue, and wake the thread
+func add_chunk(pos: Vector2i) -> void:
+	_mutex.lock()
+	_generation_queue.append(pos)
+	_mutex.unlock()
 	_semaphore.post()
--- src/world/terrain/chunk.gd
+++ src/world/terrain/chunk.gd
@@ -1,72 +1,84 @@
 class_name Chunk extends Node2D
 
-var static_body: StaticBody2D
-var visual_mesh: MeshInstance2D
-var quad_mesh: QuadMesh
+var _static_body: StaticBody2D
+var _visual_mesh: MeshInstance2D
+var _quad_mesh: QuadMesh
+var _collision_shapes: Array
 
 var terrain_data: Array = []
-var chunk_size: int # Size of the chunk in tiles
 
 
-# Constructor
-func generate(chunk_data: Array, chunk_pos: Vector2i) -> void:
+# Main thread only, sets up the data for the chunk. Use call_deferred to call this from the worker thread
+func generate(chunk_data: Array, collision_shapes: Array, chunk_pos: Vector2i) -> void:
 	terrain_data = chunk_data # Just a reference, not a copy. More memory efficient
-	chunk_size = chunk_data.size() # Assuming square chunks
-	position = Vector2(chunk_pos.x * chunk_size, chunk_pos.y * chunk_size)
+	_collision_shapes = collision_shapes
+	position = Vector2(chunk_pos.x * GlobalSettings.CHUNK_SIZE, chunk_pos.y * GlobalSettings.CHUNK_SIZE)
 
 
-# Should be called after the node is initialized and added to the tree. 
-# Should only be called once, after that use rebuild()
+# Thread safe version
+func regenerate(new_chunk_data: Array, new_collision_shapes: Array, new_chunk_pos: Vector2i) -> void:
+	set_deferred("terrain_data", new_chunk_data)
+	set_deferred("_collision_shapes", new_collision_shapes)
+	set_deferred("position", Vector2(new_chunk_pos.x * GlobalSettings.CHUNK_SIZE, new_chunk_pos.y * GlobalSettings.CHUNK_SIZE))
+
+
+# Should only be called once on the worker thread, and after generate has been called.
 func build() -> void:
 	setup_collision_shapes()
 	setup_visual_mesh()
 
 
+# Called from the main thread via call_deferred
+func rebuild() -> void:
+	call_deferred("setup_collision_shapes")
+	call_deferred("setup_visual_mesh")
+
+
 # Helper function to setup the collision shapes.
 # This starts with the shapes disabled.
 func setup_collision_shapes() -> void:
-	if static_body == null: # First time setup
-		static_body = StaticBody2D.new()
-		static_body.set_collision_mask_value(1, false)
-		add_child(static_body)
+	if _static_body == null: # First time setup - worker thread safe
+		_static_body = StaticBody2D.new()
+		_static_body.set_collision_mask_value(1, false)
+		add_child(_static_body)
 
-	# Run greedy meshing on the local copy of the terrain data
-	var shapes = GreedyMeshing.mesh(terrain_data)
-
+	# Main or worker thread safe
 	# Delete the old shapes
-	for child in static_body.get_children():
+	for child in _static_body.get_children():
 		child.queue_free()
 	# And setup the new shapes
-	for shape in shapes:
-		static_body.add_child(shape)
+	for shape in _collision_shapes:
+		shape.disabled = true # Start disabled, enable when near player
+		_static_body.add_child(shape)
 
 
 # Sets up the mesh and shader data to draw the chunk.
 func setup_visual_mesh():
-	# First time setup
-	if quad_mesh == null:
-		quad_mesh = QuadMesh.new()
-		quad_mesh.size = Vector2(chunk_size, chunk_size)
-	
-	# First time setup
-	if visual_mesh == null:
-		visual_mesh = MeshInstance2D.new()
-		visual_mesh.mesh = quad_mesh # Assign the mesh to our MeshInstance2D node
-		visual_mesh.position = visual_mesh.mesh.size / 2.0 # Center it
-		# Set up the shader material
-		visual_mesh.material = ShaderMaterial.new()
-		visual_mesh.material.shader = load("uid://b4vyqx2a5say4") # terrain.gdshader
-		visual_mesh.material.set_shader_parameter("dirt_texture", load("uid://oe2jckdihhsp")) # dirt.png
-		visual_mesh.material.set_shader_parameter("grass_texture", load("uid://83w5qniwnc00")) # grass.png
-		visual_mesh.material.set_shader_parameter("stone_texture", load("uid://dcbbq2hyi7qx3")) # stone.png
-		# And add it to the chunk scene
-		add_child(visual_mesh)
+	# First time setup - worker thread safe
+	if _quad_mesh == null:
+		_quad_mesh = QuadMesh.new()
+		_quad_mesh.size = Vector2(GlobalSettings.CHUNK_SIZE, GlobalSettings.CHUNK_SIZE)
 
+	# First time setup - worker thread safe
+	if _visual_mesh == null:
+		_visual_mesh = MeshInstance2D.new()
+		_visual_mesh.mesh = _quad_mesh # Assign the mesh to our MeshInstance2D node
+		_visual_mesh.position = _visual_mesh.mesh.size / 2.0 # Center it
+		# Set up the shader material
+		_visual_mesh.material = ShaderMaterial.new()
+		_visual_mesh.material.shader = load("uid://b4vyqx2a5say4") # terrain.gdshader
+		_visual_mesh.material.set_shader_parameter("dirt_texture", load("uid://oe2jckdihhsp")) # dirt.png
+		_visual_mesh.material.set_shader_parameter("grass_texture", load("uid://83w5qniwnc00")) # grass.png
+		_visual_mesh.material.set_shader_parameter("stone_texture", load("uid://dcbbq2hyi7qx3")) # stone.png
+		# And add it to the chunk scene
+		add_child(_visual_mesh)
+
+	# Main or worker thread safe
 	# This is called every time `setup_visual_mesh` is called to rebuild the chunk visuals
 	# Loop over the terrain data to create the image for the shader
-	var image = Image.create(chunk_size, chunk_size, false, Image.FORMAT_RGBA8) # May need to play with this later to encode different types of data into the shader
-	for x in range(chunk_size):
-		for y in range(chunk_size):
+	var image = Image.create(GlobalSettings.CHUNK_SIZE, GlobalSettings.CHUNK_SIZE, false, Image.FORMAT_RGBA8) # May need to play with this later to encode different types of data into the shader
+	for x in range(GlobalSettings.CHUNK_SIZE):
+		for y in range(GlobalSettings.CHUNK_SIZE):
 			# Encode the terrain data into the color channels
 			var tile_id = float(terrain_data[-y - 1][x][0]) # 0th index is tile id
 			var cell_id = float(terrain_data[-y - 1][x][1]) # 1st index is the cell the tile belongs to
@@ -75,16 +87,18 @@ func setup_visual_mesh():
 			image.set_pixel(x, y, pixel_data)
 
 	var data_texture = ImageTexture.create_from_image(image)
-	visual_mesh.material.set_shader_parameter("chunk_data_texture", data_texture)
+	_visual_mesh.material.set_shader_parameter("chunk_data_texture", data_texture)
 
 
+# Main thread only
 # Disables the collision shapes 
 func disable_collision() -> void:
-	for child in static_body.get_children():
+	for child in _static_body.get_children():
 		child.set_deferred("disabled", true)
 
 
+# Main thread only
 # Enables the collision shapes
 func enable_collision() -> void:
-	for child in static_body.get_children():
+	for child in _static_body.get_children():
 		child.set_deferred("disabled", false)
